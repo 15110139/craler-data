595b95b4f80520e12e9ff653cf8aef83350a1782
15110139 <000big0bang000@gmail.com>
2019-03-24 13:08:29 +0700

    tien

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var cheerio = require('cheerio');
 var rp = require('request-promise');
+var uuid = require('uuid');
+const elasticsearch = require('elasticsearch');
 
+const client = new elasticsearch.Client({
+	host: 'localhost:9200',
+	log: 'trace'
+});
 async function crawler() {
 	let listCopany = [];
-	for (i = 0; i <= 0; i++) {
+	for (i = 0; i <= 56; i++) {
 		try {
 			option = {
 				type: 'POST',
@@ -13,22 +19,19 @@ async function crawler() {
 					lang_search: 'en',
 					page_search: i,
 					title_search: '',
-					location: '',
+					location: ''
 				},
 
 				headers: {
-					'Content-type': 'application/x-www-form-urlencoded',
-				},
+					'Content-type': 'application/x-www-form-urlencoded'
+				}
 			};
 			const data = await rp('https://www.topitworks.com/vi/company/ajaxlistcompany', option);
 			const res = JSON.parse(data);
 			const body = res.data;
-			const totalPage = res.totalPage;
-			const currentPage = res.currentPage;
 			const $ = cheerio.load(body);
 			await $('.col-xs-12, .col-md-4, .col-sm-6').each(async (index, el) => {
 				const jod = cheerio.load(el);
-				console.log(jod('a').attr('href'));
 				const urlCompany = jod('a').attr('href');
 				listCopany.push(urlCompany);
 				// await crawlerCompay(urlCompany);
@@ -42,98 +45,78 @@ async function crawler() {
 
 	console.log(listCopany.length);
 	console.log('0k');
+	var bulk = [];
 
-	for (i = 11; i <= 11; i++) {
+	for (i = 0; i <= 175; i++) {
 		try {
 			console.log('------------------------------------------------', i, listCopany[i]);
+			const cp = {};
+			cp._id = uuid.v1();
 			const data = await rp(listCopany[i]);
 			const $ = cheerio.load(data);
 			const cp_company_name = $('#cp_company_name');
-			console.log(cp_company_name);
 			console.log(cp_company_name.text());
 			const company_type = $('.company_type');
 			if (company_type.data() !== undefined) {
 				console.log(company_type.text());
+				cp.company_type = company_type.text();
 			}
 			console.log('------------technologies--------------');
 			if ($('.cp_key_technologies').data() !== undefined) {
 				const cp_key_technologies = cheerio.load($('.cp_key_technologies').html());
+				let tec = '';
 				cp_key_technologies('ul li').each((index, el) => {
 					console.log(cheerio.load(el).text());
+					tec = tec + ' ' + cheerio.load(el).text();
 				});
+				cp.technologies = tec;
+				console.log(tec);
 			}
 			console.log('-------------cp_basic_info_details--------------------');
 			if ($('.cp_basic_info_details').data() !== undefined) {
+				let location = '';
 				const cp_basic_info_details = cheerio.load($('.cp_basic_info_details').html());
 				cp_basic_info_details('.li-items-limit').each((index, el) => {
 					console.log(cheerio.load(el).text());
+					location = location + ' ' + cheerio.load(el).text();
 				});
+				cp.location = location;
 			}
-			console.log('-------------jod--------------------');
-			const cp_our_job_item = $('.cp_our_job_item');
-			if (cp_our_job_item.data() !== undefined) {
-				cp_our_job_item.each((index, el) => {
-					const job = cheerio.load(el);
-					console.log(job('h4 a').text());
-					job('ul li').each((index, el) => {
-						console.log(cheerio.load(el).text().trim());
-					});
-				});
-			}
+			console.log('---------------------------------------');
+			console.log(cp);
+			// console.log('-------------jod--------------------');
+			// const cp_our_job_item = $('.cp_our_job_item');
+			// if (cp_our_job_item.data() !== undefined) {
+			// 	cp_our_job_item.each((index, el) => {
+			// 		const job = cheerio.load(el);
+			// 		console.log(job('h4 a').text());
+			// 		job('ul li').each((index, el) => {
+			// 			console.log(cheerio.load(el).text().trim());
+			// 		});
+			// 	});
+			// }
+			bulk.push({
+				index: {
+					_index: 'job',
+					_type: 'company'
+				}
+			});
+			bulk.push(cp);
+
+			//perform bulk indexing of the data passed
 		} catch (error) {
 			console.log(error);
 		}
+
 	}
 
-	// let page = 0;
-	// const option = {
-	// 	url: 'https://www.topitworks.com/vi/company/ajaxlistcompany',
-	// 	type: 'POST',
-
-	// 	method: 'POST',
-	// 	form: {
-	// 		lang_search: 'en',
-	// 		page_search: 2,
-	// 		title_search: '',
-	// 		location: '',
-	// 	},
-
-	// 	headers: {
-	// 		'Content-type': 'application/x-www-form-urlencoded',
-	// 	},
-	// };
-	// while (true) {
-	// 	try {
-	// 		const data = await rp(option);
-	// 		const res = JSON.parse(data);
-	// 		const body = res.data;
-	// 		const totalPage = res.totalPage;
-	// 		const currentPage = res.currentPage;
-	// 		const $ = cheerio.load(body);
-	// 		await $('.col-xs-12, .col-md-4, .col-sm-6').each(async (index, el) => {
-	// 			console.log(index);
-	// 			const jod = cheerio.load(el);
-	// 			console.log(jod('a').attr('href'));
-	// 			const urlCompany = jod('a').attr('href');
-	// 			// await crawlerCompay(urlCompany);
-	// 			// });
-	// 		});
-	// 		page = page + 1;
-	// 		// console.log('page', page);
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 		break;
-	// 	}
-	// }
-}
-
-async function crawlerCompay(urlCompany) {
-	try {
-		const chirdboyd = await rp(urlCompany);
-		console.log(chirdboyd);
-	} catch (error) {
-		throw error;
-	}
+	client.bulk({ body: bulk }, function(err, response) {
+		if (err) {
+			console.log('Failed Bulk operation'.red, err);
+		} else {
+			console.log('Successfully imported %s', bulk.length);
+		}
+	});
 }
 
 crawler();
